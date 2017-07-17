3a762da440f0bbd6b6da7bcadbbf4c97bb0349b6
Till Grallert <till.grallert@fu-berlin.de>
2017-07-18 01:43:50 +0200

    added a change of classes

--- js/nav-slideout.js
+++ js/nav-slideout.js
@@ -1,9 +1,13 @@
 function openNav() {
     document.getElementById("navigation").style.width = "300px";
     document.getElementById("sidebar-buttons").style.right = "300px";
+    document.getElementById("menuOpen").className = "c_hidden";
+    document.getElementById("menuClose").className = "c_visible";
 }
 
 function closeNav() {
     document.getElementById("navigation").style.width = "0";
     document.getElementById("sidebar-buttons").style.right = "0";
+    document.getElementById("menuClose").className = "c_hidden";
+    document.getElementById("menuOpen").className = "c_visible";
 }
