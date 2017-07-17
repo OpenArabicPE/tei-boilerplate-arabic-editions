01f533fa43b1892786b7b901ab97ad74e15aae7c
Till Grallert <till.grallert@fu-berlin.de>
2017-07-18 01:50:24 +0200

    changed type of comment

--- js/nav-slideout.js
+++ js/nav-slideout.js
@@ -1,8 +1,9 @@
 function openNav() {
     document.getElementById("navigation").style.width = "300px";
     document.getElementById("sidebar-buttons").style.right = "300px";
-    /*document.getElementById("menuOpen").className = "c_hidden";
-    document.getElementById("menuClose").className = "c_visible";*/
+//    document.getElementById("content").style.right = "300px";
+//    document.getElementById("menuOpen").className = "c_hidden";
+//    document.getElementById("menuClose").className = "c_visible";
     document.getElementById("menuOpen").style.visibility = "hidden";
     document.getElementById("menuClose").style.visibility = "visible";
 }
@@ -10,6 +11,7 @@ function openNav() {
 function closeNav() {
     document.getElementById("navigation").style.width = "0";
     document.getElementById("sidebar-buttons").style.right = "0";
+//    document.getElementById("content").style.right = "0";
     document.getElementById("menuClose").style.visibility = "hidden";
     document.getElementById("menuOpen").style.visibility = "visible";
 }
