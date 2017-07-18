22492dad0305117727fa4316bb67f9e5f13fca94
Till Grallert <till.grallert@fu-berlin.de>
2017-07-19 00:50:49 +0200

    now the entire content will move to the left

--- js/nav-slideout.js
+++ js/nav-slideout.js
@@ -1,17 +1,32 @@
 function openNav() {
-    document.getElementById("navigation").style.width = "300px";
-    document.getElementById("sidebar-buttons").style.right = "300px";
-//    document.getElementById("content").style.right = "300px";
-//    document.getElementById("menuOpen").className = "c_hidden";
-//    document.getElementById("menuClose").className = "c_visible";
-    document.getElementById("menuOpen").style.visibility = "hidden";
-    document.getElementById("menuClose").style.visibility = "visible";
+    styleWidth('navigation','300');
+    styleRight('sidebar-buttons','300');
+    document.getElementById("body").style.marginRight = "300px";
+    document.getElementById("body").style.marginLeft = "-300px";
+    styleVisibility('menuOpen','hidden');
+    styleVisibility('menuClose','visible');
+    // document.getElementById("menuOpen").style.visibility = "hidden";
+    // document.getElementById("menuClose").style.visibility = "visible";
 }
 
 function closeNav() {
-    document.getElementById("navigation").style.width = "0";
-    document.getElementById("sidebar-buttons").style.right = "0";
-//    document.getElementById("content").style.right = "0";
-    document.getElementById("menuClose").style.visibility = "hidden";
-    document.getElementById("menuOpen").style.visibility = "visible";
+    styleWidth('navigation','0');
+    styleRight('sidebar-buttons','0');
+    document.getElementById("body").style.marginRight = "0px";
+    document.getElementById("body").style.marginLeft = "0px";
+    styleVisibility('menuOpen','visible');
+    styleVisibility('menuClose','hidden');
+    // document.getElementById("menuClose").style.visibility = "hidden";
+    // document.getElementById("menuOpen").style.visibility = "visible";
+}
+
+// set the width of an element by ID
+function styleWidth(tag,pixels) {
+	document.getElementById(tag).style.width = pixels + "px";
+}
+function styleRight(tag,pixels) {
+	document.getElementById(tag).style.right = pixels + "px";
+}
+function styleVisibility(tag,value) {
+	document.getElementById(tag).style.visibility = value;
 }
