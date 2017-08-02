2efb0c7663cedebd1cbd08b3701caf32f9ab678a
Till Grallert <till.grallert@fu-berlin.de>
2017-08-02 20:16:59 +0300

    added jquery for toggle line breaks

--- js/script.js
+++ js/script.js
@@ -7,9 +7,81 @@ function toggleLineBeginnings() {
 	console.log("You pressed me?")
 	// generate a variable holding <br/> elements
 	var theBreaks = document.getElementsByTagName('lb');
+	// console.log(theBreaks)
+	theBreaks.innerHTML = "hello world!";
 	console.log(theBreaks)
-	theBreaks.innerHTML = "<br />"
 }
 
 // do something upon clicking the button
-theButton.addEventListener('click',toggleLineBeginnings);
+//theButton.addEventListener('click',toggleLineBeginnings);
+
+function insertHTML_ByXPath( xpath, position, newElement) {
+    var element = document.evaluate(xpath, window.document, null, 9, null ).singleNodeValue;
+    element.insertAdjacentHTML(position, newElement);
+    // element.style='border:3px solid orange';
+}
+
+var xpath_DOMElement = '//*[@id="answer-33669996"]/table/tbody/tr[1]/td[2]/div';
+var childHTML = '<div id="Yash">Hi My name is <B>\"YASHWANTH\"</B></div>';
+var position = 'beforeend';
+
+// insertHTML_ByXPath(xpath_DOMElement, position, childHTML);
+
+// do something upon clicking the button
+// theButton.addEventListener('click',insertHTML_ByXPath('//lb[1]',position,'<span>hello world</span>'))
+
+function addCSS(tag) {
+    window.document.getElementsByTagName(tag)[1].style.cssText += "display: block;";
+}
+//theButton.addEventListener('click',addCSS('lb'));
+
+function addElement(tag, addedElement, style, innerHTML){
+    var elemDiv = document.createElement(addedElement);
+    // elemDiv.style.cssText = style;
+    elemDiv.style.cssText = 'width:100%;height:10%;background:rgb(192,192,192);';
+    elemDiv.innerHTML = innerHTML; 
+    window.document.body.insertBefore(elemDiv, window.document.body.getElementsByTagName(tag));
+    // document.body.appendChild(elemDiv); // appends last of that element
+}
+
+//theButton.addEventListener('click',addElement('lb','span','display:block;','test'))
+
+
+// we could add jquery functions
+// Event setup using a convenience method
+$( "#b_toggle-lb" ).click(function() {
+    // change the class of the button
+    $( "#b_toggle-lb" ).toggleClass( "c_toggled" );
+    // change the class of all line breaks to "c_toggled". The display is then set to "display:block" in CSS
+    $("lb").toggleClass("c_toggled");
+    // or change <lb> to <br>. 
+    // $("lb").replaceWith("<br></br>")
+    // check class of button 
+    if ($("#b_toggle-lb").is(".c_toggled")) {
+        // if button is toggled do some thing
+        console.log("toggled on");
+        // change the button content
+        $("#b_toggle-lb").text("toogle line breaks off")
+    }
+    else {
+        // revert to original state
+        console.log("toggled off");
+        // change the button content
+        $("#b_toggle-lb").text("toogle line breaks on")
+    }
+});
+
+// every button should get the "c_toggled class
+$("button").on("click", function(){
+    $("button").toggleClass("c_toggled");
+    var id = $("button").attr('id');
+    // check class of button 
+    if ($("button").is(".c_toggled")) {
+        // if button is toggled do some thing
+        console.log(id += " toggled on");
+    }
+    else {
+        // revert to original state
+        console.log(id += " toggled off");
+    }
+});
